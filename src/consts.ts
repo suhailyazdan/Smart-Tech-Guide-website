18c89dad9b01b793f512d501092e5fc20ae020ba
suhailyazdan <yazdan.suhail@gmail.com>
2026-06-17 10:27:14 +0000

    added codespace - tested

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Smart Tech Guide - Blog site';
+export const SITE_DESCRIPTION = 'Welcome to my Smart Tech Guide website!';
